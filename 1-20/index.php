0c75086a30af1fc7c45a5a83e5aaf606f34e3688
kaakeli <60170508+kaakeli@users.noreply.github.com>
2020-10-06 14:44:42 +0300

    Update index.php
    1

--- 1-20/index.php
+++ 1-20/index.php
@@ -1,1 +1,21 @@
 <?php
+$rows = file(__DIR__ . '/maakunnat.txt');
+//print_r();
+$list = [];
+foreach ($rows as $row) {
+    $part = explode(';', $row);
+    $list[$parts[0] ] = (int) $parts[1];
+
+   
+}
+arsort($list);
+$text = "";
+foreach ($list as $maakunnat => $vakiluku) {
+    if($vakiluku % 2 !== 0)
+    {
+        $text .= "$maakunta,$vakiluku\n";
+    }
+    file_put_contents(__DIR__ . '/maakunnat_vastaus.txt', $text );
+}
+
+?>
